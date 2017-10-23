76492dc2f5adeecbbf05c77667718405cdc3a9a3
Дмитрий Тюрев <dmitry_tyurev@playrix.com>
2017-10-23 23:18:01 +0300

    Рефакторинг

--- LearnWords/FileOperate.h
+++ LearnWords/FileOperate.h
@@ -9,6 +9,7 @@ struct FileOperate
 	static void save_to_file(const char* fullFileName, WordsData* pWordsData);
 	static void export_for_google_doc(WordsData* pWordsData);
 
+private:
 	static std::string load_string_from_array(const std::vector<char>& buffer, int* indexToRead);
 	static int load_int_from_array(const std::vector<char>& buffer, int* indexToRead);
 };
