cf375a1e450e948ba7db011ac1cebd718905231c
Дмитрий Тюрев <dmitry_tyurev@playrix.com>
2018-04-28 22:56:39 +0300

    "Revert "Из режима повтора слов по времени, убрана вставка рандомных слов"" This reverts commit 49fbb4d972f6c73951f9f3d329a3ab3be0337e5c.

--- LearnWords/MandatoryCheck.cpp
+++ LearnWords/MandatoryCheck.cpp
@@ -18,47 +18,13 @@ extern Log logger;
 // 
 //===============================================================================================
 
-float MandatoryCheck::calc_additional_word_probability(int checkByTimeWordsNumber)
-{
-	const float MIN_PROB = 0.1f;
-	const float MAX_PROB = 0.9f;
-	const float MIN_WORDS = 25;
-	const float MAX_WORDS = 50;
-
-	float prob = interp(MAX_WORDS, MIN_PROB, MIN_WORDS, MAX_PROB, (float)checkByTimeWordsNumber);   // –ассчитаем веро€тность вставки доп. слов в зависимости от числа слов дл€ об€зательной проверки по времени
-	prob = std::max(MIN_PROB, prob);
-	prob = std::min(MAX_PROB, prob);
-	return prob;
-}
-
-
-//===============================================================================================
-// 
-//===============================================================================================
-
 
 void MandatoryCheck::mandatory_check(time_t freezedTime, AdditionalCheck* pAdditionalCheck, const std::string& fullFileName)
 {
-	enum class FromWhatSource
-	{
-		DEFAULT,
-		CHECK_BY_TIME,
-		RANDOM_REPEAT,
-	};
-
-	struct WordToCheck
-	{
-		WordToCheck() : _index(0), _fromWhatSource(FromWhatSource::DEFAULT) {}
-		WordToCheck(int index, FromWhatSource fromWhatSource) : _index(index), _fromWhatSource(fromWhatSource) {}
-
-		int  _index;                     // »ндекс повтор€емого слова в WordsOnDisk::_words  (если _fromWhatSource == RANDOM_REPEAT, то здесь всегда 0)
-		FromWhatSource _fromWhatSource;  // “ип слова
-	};
-
 	_learnWordsApp->clear_forgotten();
 	clear_console_screen();
 
-	std::vector<WordToCheck> wordsToRepeat;
+	std::vector<int> wordsToRepeat;
 
 	// ¬ыбрать слова дл€ проверки, дл€ которых подошло врем€ проверки
 
@@ -66,38 +32,15 @@ void MandatoryCheck::mandatory_check(time_t freezedTime, AdditionalCheck* pAddit
 	{
 		WordsData::WordInfo& w = _pWordsData->_words[i];
 		if (w.dateOfRepeat != 0 && w.dateOfRepeat < freezedTime)
-		{
-			WordToCheck wordToCheck(i, FromWhatSource::CHECK_BY_TIME);
-			wordsToRepeat.push_back(wordToCheck);
+			wordsToRepeat.push_back(i);
 		}
-	}
 	std::random_shuffle(wordsToRepeat.begin(), wordsToRepeat.end());
-
-	// ћежду слов, которые нужно проверить по времени, повставл€ем слова дл€ случайной проверки
-	float prob = calc_additional_word_probability((int)wordsToRepeat.size());
-
-	for (int i = 0; i < (int)wordsToRepeat.size() - 1; ++i)
-	{
-		if (rand_float(0, 1) < prob)
-		{
-			WordToCheck wordToCheck(0, FromWhatSource::RANDOM_REPEAT);   // »ндекс слова не записываем, только его тип! »ндекс получим по ходу проверки
-			wordsToRepeat.insert(wordsToRepeat.begin() + i + 1, wordToCheck);
-			++i;
-		}
-	}
 
 	// √лавный цикл проверки слов
 
 	for (int i = 0; i < (int)wordsToRepeat.size(); ++i)
 	{
-		if (wordsToRepeat[i]._fromWhatSource == FromWhatSource::RANDOM_REPEAT)
-		{
-			wordsToRepeat[i]._index = pAdditionalCheck->get_word_to_repeat(freezedTime);
-			if (wordsToRepeat[i]._index == -1)
-				continue;
-		}
-
-		WordsData::WordInfo& w = _pWordsData->_words[wordsToRepeat[i]._index];
+		WordsData::WordInfo& w = _pWordsData->_words[wordsToRepeat[i]];
 
 		clear_console_screen();
 		printf("\n\n===============================\n %s\n===============================\n", w.word.c_str());
@@ -121,7 +64,7 @@ void MandatoryCheck::mandatory_check(time_t freezedTime, AdditionalCheck* pAddit
 			_learnWordsApp->print_buttons_hints(w.translation, true);
 			printf("\n  ќсталось: %d, Ѕыстрый ответ = %d, rightAnswersNum=%d\n", (int)wordsToRepeat.size() - i - 1, int(isQuickAnswer), w.rightAnswersNum);
 
-			CloseTranslationWordsManager ctwm(_learnWordsApp, _pWordsData, wordsToRepeat[i]._index);
+			CloseTranslationWordsManager ctwm(_learnWordsApp, _pWordsData, wordsToRepeat[i]);
 			ctwm.print_close_words_by_translation();
 
 			c = getch_filtered();
@@ -131,8 +74,6 @@ void MandatoryCheck::mandatory_check(time_t freezedTime, AdditionalCheck* pAddit
 			int keepPrevRightAnswersNum = w.rightAnswersNum;
 			if (c == 72)  // —трелка вверх
 			{
-				if (wordsToRepeat[i]._fromWhatSource == FromWhatSource::CHECK_BY_TIME)
-				{
 					pAdditionalCheck->put_word_to_end_of_random_repeat_queue_common(w);
 					LearnWordsApp::RandScopePart randScopePart = LearnWordsApp::RandScopePart::LOWER_PART;
 					if (isQuickAnswer)
@@ -147,54 +88,29 @@ if (w.rightAnswersNum - keep > 1)
 {
 	clear_console_screen();
 	printf("Moved to %d!\n", w.rightAnswersNum - keep);
-	Sleep(3000);
+	Sleep(1200);
 }
 					_learnWordsApp->fill_dates_and_save(w, freezedTime, randScopePart);
 				}
 				else
-				{
-					pAdditionalCheck->put_word_to_end_of_random_repeat_queue_common(w);
-					if (isQuickAnswer)
-						w.isNeedSkipOneRandomLoop = true;
-					_learnWordsApp->save();
-				}
-			}
-			else
 				if (c == 80) // —трелка вниз
 				{
-					if (wordsToRepeat[i]._fromWhatSource == FromWhatSource::CHECK_BY_TIME)
-					{
-						_learnWordsApp->add_forgotten(wordsToRepeat[i]._index);
+					keepPrevRightAnswersNum = w.rightAnswersNum;
+					_learnWordsApp->add_forgotten(wordsToRepeat[i]);
 						_learnWordsApp->set_word_as_just_learned(w);
 						_learnWordsApp->fill_dates_and_save(w, freezedTime, LearnWordsApp::RandScopePart::ALL);
 					}
 					else
-					{
-						_learnWordsApp->add_forgotten(wordsToRepeat[i]._index);
-						_learnWordsApp->set_word_as_just_learned(w);
-						_learnWordsApp->fill_dates_and_save(w, freezedTime, LearnWordsApp::RandScopePart::ALL);
-					}
-				}
-				else
 					if (c == 77) // —трелка вправо (помним слово не очень уверенно)
 					{
-						if (wordsToRepeat[i]._fromWhatSource == FromWhatSource::CHECK_BY_TIME)
-						{
-							_learnWordsApp->add_forgotten(wordsToRepeat[i]._index);
+						_learnWordsApp->add_forgotten(wordsToRepeat[i]);
 							w.rightAnswersNum = std::min(w.rightAnswersNum, RIGHT_ANSWERS_FALLBACK);
 							pAdditionalCheck->put_word_to_end_of_random_repeat_queue_fast(w, freezedTime);
 							_learnWordsApp->fill_dates_and_save(w, freezedTime, LearnWordsApp::RandScopePart::ALL);
 						}
 						else
-						{
-							_learnWordsApp->add_forgotten(wordsToRepeat[i]._index);
-							pAdditionalCheck->put_word_to_end_of_random_repeat_queue_fast(w, freezedTime);
-							_learnWordsApp->save();
-						}
-					}
-					else
 						continue;
-			logger("Check by time, word = %s, ===== %s, src=%d, key=%d, PrevRightAnswersNum=%d, time = %s", w.word.c_str(), fullFileName.c_str(), wordsToRepeat[i]._fromWhatSource, c, keepPrevRightAnswersNum, get_time_in_text(time(nullptr)));
+			logger("Check by time, word = %s, ===== %s, key=%d, PrevRightAnswersNum=%d, time = %s", w.word.c_str(), fullFileName.c_str(), c, keepPrevRightAnswersNum, get_time_in_text(time(nullptr)));
 			break;
 		}
 	}
